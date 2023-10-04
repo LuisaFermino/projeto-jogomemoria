e24a12aef71ee903129e6c5b2a42594b08326512
LuisaFermino <souzafermino49@gmail.com>
2023-10-04 11:32:25 -0300

    Implementação de versos e efeitos para virar carta

--- js/scripts.js
+++ js/scripts.js
@@ -8,20 +8,30 @@ function adicionarCartas(qtdCartas) {
   const elemento = document.querySelector(".container-cards");
   elemento.innerHTML =
     elemento.innerHTML +
-    `<div class="card">
-        <div class="front-face">  <img src="./img/front.png" class="img-card" /></div>
+    `<div class="card" onclick="virarCarta()">
+      <div class="front face">${versoTeste}</div>
+      <div class="back face"></div>
     </div>`;
 }
 
-for(let i = 0; i < qtdCartas ;i++){
+for(let i = 0; i < qtdCartas/2 ;i++){
   adicionarCartas();
 }
 
+for(let i = 0; i < qtdCartas/2 ;i++){
+  adicionarCartas();
+}
 
-// versos = [
-//  img 1
-//  img 2
-//  img 3
+// const versosCarta = [
+//   `<img src="./img/unicornparrot.gif" class="img-card" />`,
+//   `<img src="./img/triplestsparrot.gif" class="img-card" />`,
+//   `<img src="./img/revertitparrot.gif" class="img-card" />`,
+//   `<img src="./img/metalparrot.gif" class="img-card" />`,
+//   `<img src="./img/fiestaparrot.gif" class="img-card" />`,
+//   `<img src="./img/explodyparrot.gif" class="img-card" />`,
+//   `<img src="./img/bobrossparrot.gif" class="img-card" />`
+
 // ]
 
-// versos[i]
+
+const versoTeste = ["lulu", "Paulão"]
