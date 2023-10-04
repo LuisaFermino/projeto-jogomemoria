083a4f8a15eccb32b9eb5890b04d1ea3cb6615ce
LuisaFermino <souzafermino49@gmail.com>
2023-10-04 17:02:47 -0300

    Novos testes de colocação de cartas

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,42 @@ let qtdCartas = 1;
 while (qtdCartas < 4 || qtdCartas > 14 || qtdCartas % 2 !== 0 || typeof(qtdCartas)  === "string") {
   qtdCartas = parseInt(prompt("Quantas cartas você deseja? (numeros pares de 4 a 14)"));
 }
+const testeVersos = ["lulu", "socorro", "apululu", "aiaia", "gracinha", "vamori", "ultimo"];
 
 function adicionarCartas(qtdCartas) {
-  const elemento = document.querySelector(".container-cards");
-  elemento.innerHTML =
-    elemento.innerHTML +
-    `<div class="card" onclick="virarCarta()">
-      <div class="front face">${versoTeste}</div>
+  // const elemento = document.querySelector(".container-cards");
+  // elemento.innerHTML =
+  //   elemento.innerHTML +
+  //   `<div class="card" onclick="virarCarta()">
+  //     <div class="front face"></div>
+  //     <div class="back face"></div>
+  //   </div>`;
+  
+  const cards = document.querySelector(".container-cards");
+  for(let i = 0; i < qtdCartas; i++){
+    cards.innerHTML =  cards.innerHTML +`<div class="card" onclick="virarCarta()">
+      <div class="front face"></div>
       <div class="back face"></div>
-    </div>`;
+    </div>
+    `
+
+}
 }
 
-for(let i = 0; i < qtdCartas/2 ;i++){
+
+for(let i = 0; i < testeVersos.length ;i++){
   adicionarCartas();
+ 
 }
 
-for(let i = 0; i < qtdCartas/2 ;i++){
-  adicionarCartas();
-}
+// for(let i = 0; i < qtdCartas/2;i++){
+//   adicionarCartas();
+// }
+
+
+
+
+
 
 // const versosCarta = [
 //   `<img src="./img/unicornparrot.gif" class="img-card" />`,
@@ -34,4 +52,4 @@ for(let i = 0; i < qtdCartas/2 ;i++){
 // ]
 
 
-const versoTeste = ["lulu", "Paulão"]
+
